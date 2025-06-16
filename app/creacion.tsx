e92cec295daa41e50e1af6a3b90067f06abf4455
Luna Hazuki <lunahazuki2006@outlook.com>
2025-06-16 12:19:47 -0400

    formateo

--- app/creacion.tsx
+++ app/creacion.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
 
 export default function Creacion() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Aquí se crearía una tarea.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Aquí se crearía una tarea.</Text>
+        </View>
+    );
 }
