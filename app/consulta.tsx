c902afeef74d8765b8dc6b7f91d8c568e93ee562
Luna Hazuki <lunahazuki2006@outlook.com>
2025-06-16 10:17:57 -0400

    Con las páginas que se usarán (super básicos)

---
+++ app/consulta.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Consulta() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Aquí se consultaría una tarea.</Text>
+    </View>
+  );
+}
